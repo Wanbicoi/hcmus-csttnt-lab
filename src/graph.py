117e97aea60b3e4e694016f553d87a23781ea71c
Pi ne <wanbicoi123@gmail.com>
2023-10-19 18:04:34 +0700

    update

--- src/graph.py
+++ src/graph.py
@@ -5,8 +5,8 @@ class Graph:
     # có gì đó sai sai, chưa xử lý trường hợp nếu point là điểm thưởng
     def add_edge(self, pointA, pointB, weight):
         if pointA in self.graph:
-            self.graph[pointA].append((pointB, weight))
-            self.graph[pointB].append((pointA, weight))
+            self.graph[str(pointA)].append((pointB, weight))
+            self.graph[str(pointB)].append((pointA, weight))
         else:
-            self.graph[pointA] = [pointA, weight]
-            self.graph[pointB] = [pointB, weight]
+            self.graph[str( pointA )] = [pointA, weight]
+            self.graph[str(pointB)] = [pointB, weight]
